177df6e250bd769c5c9b7c1d11b250c7f0028145
Brian-Wuest <Brian.Wuest@outlook.com>
2017-10-02 16:54:31 -0500

    Adding powershell build script.

---
+++ prod_build.ps1
@@ -0,0 +1,7 @@
+$currentPath = (Resolve-Path .\).Path
+$buildPath = (Resolve-Path ..\Brian-Wuest.github.io).Path
+
+Get-ChildItem $buildPath -Include *.js -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.css -Recurse | foreach ($_) {Remove-Item $_.FullName}
+
+ng build -prod --output-path=$buildPath --delete-output-path=false --aot
