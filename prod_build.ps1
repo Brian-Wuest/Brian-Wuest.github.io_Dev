a964de0a5e99890c9f40b43a8851b540e8e841ce
Brian Wuest <brian.wuest@outlook.com>
2019-08-22 06:11:03 -0500

    Adding more types of files to delete

--- prod_build.ps1
+++ prod_build.ps1
@@ -3,5 +3,12 @@ $buildPath = (Resolve-Path ..\Brian-Wuest.github.io).Path
 
 Get-ChildItem $buildPath -Include *.js -Recurse | foreach ($_) {Remove-Item $_.FullName}
 Get-ChildItem $buildPath -Include *.css -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.eot -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.gif -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.png -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.svg -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.ttf -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.woff -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.woff2 -Recurse | foreach ($_) {Remove-Item $_.FullName}
 
 ng build --prod --output-path=$buildPath
