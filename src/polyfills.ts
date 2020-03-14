c5252c17458e0af385367e2b2ba65e780de9662e
Brian Wuest <brian.wuest@outlook.com>
2020-03-14 18:23:37 -0500

    Re-formatting file

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -15,15 +15,13 @@
  */
 
 /***************************************************************************************************
-* BROWSER POLYFILLS
-*/
+ * BROWSER POLYFILLS
+ */
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
