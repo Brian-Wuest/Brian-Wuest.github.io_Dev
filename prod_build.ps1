af639ad5e509ba33fd806f59b57a761fb518fb54
Brian Wuest <brian.wuest@outlook.com>
2020-03-14 18:23:25 -0500

    Fixing production builds

--- prod_build.ps1
+++ prod_build.ps1
@@ -1,14 +1,16 @@
 $currentPath = (Resolve-Path .\).Path
 $buildPath = (Resolve-Path ..\Brian-Wuest.github.io).Path
 
-Get-ChildItem $buildPath -Include *.js -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.css -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.eot -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.gif -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.png -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.svg -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.ttf -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.woff -Recurse | foreach ($_) {Remove-Item $_.FullName}
-Get-ChildItem $buildPath -Include *.woff2 -Recurse | foreach ($_) {Remove-Item $_.FullName}
+Get-ChildItem $buildPath -Include *.js -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.css -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.eot -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.gif -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.png -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.svg -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.ttf -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.woff -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.woff2 -Recurse | foreach ($_) { Remove-Item $_.FullName }
 
-ng build --prod --output-path=$buildPath
+ng build --prod
+
+Get-ChildItem .\dist -Recurse | foreach ($_) { Copy-Item -Path $_.FullName -Destination $buildPath -Force }
