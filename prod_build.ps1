808e5e3a5dfb2b4047f1664229eb2e8f54228027
Brian Wuest <brian.wuest@outlook.com>
2020-06-03 06:31:56 -0500

    updating script to remove linting issues

--- prod_build.ps1
+++ prod_build.ps1
@@ -1,16 +1,15 @@
-$currentPath = (Resolve-Path .\).Path
 $buildPath = (Resolve-Path ..\Brian-Wuest.github.io).Path
 
-Get-ChildItem $buildPath -Include *.js -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.css -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.eot -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.gif -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.png -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.svg -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.ttf -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.woff -Recurse | foreach ($_) { Remove-Item $_.FullName }
-Get-ChildItem $buildPath -Include *.woff2 -Recurse | foreach ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.js -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.css -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.eot -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.gif -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.png -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.svg -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.ttf -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.woff -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
+Get-ChildItem $buildPath -Include *.woff2 -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName }
 
 ng build --prod
 
-Get-ChildItem .\dist -Recurse | foreach ($_) { Copy-Item -Path $_.FullName -Destination $buildPath -Force }
+Get-ChildItem .\dist -Recurse | ForEach-Object ($_) { Copy-Item -Path $_.FullName -Destination $buildPath -Force }
